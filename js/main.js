4ceb6297a10ae6916bf61751a920cac64b1770e0
junhee <wnsgml972@naver.com>
2018-03-05 04:12:21 +0900

    Update Directory

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 hljs.initHighlightingOnLoad();
 
 $(document).ready(function(){
+
     $("h2,h3,h4,h5,h6").each(function(i,item){
        var tag = $(item).get(0).localName;
        $(item).attr("id","dir"+i);
-       $("#category").append('<a class="new'+tag+'" href="#dir'+i+'">'+$(this).text()+'</a></br>');
+       $("#category").append('<a style="color:black;" class="new'+tag+'" href="#dir'+i+'">'+$(this).text()+'</a></br>');
        $(".newh2").css("margin-left",25);
        $(".newh3").css("margin-left",45);
        $(".newh4").css("margin-left",65);
@@ -12,12 +13,11 @@ $(document).ready(function(){
        $(".newh6").css("margin-left",105);
       });
 
-      $("#category").hide();
+    $("#category").hide();
 });
 
 $("#dir_control").click(function() {
-  alert("확인");
-	$("#dir_control").toggle(function() {
+	$("#category").toggle(function() {
 		$("#dir_control").toggleClass("fa fa-plus fa fa-minus")
 	})
 });
