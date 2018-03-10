3ab7d3972f08e9e4a22009346775e44f28466730
junhee <wnsgml972@naver.com>
2018-03-11 02:52:48 +0900

    Update Total Posts

--- js/main.js
+++ js/main.js
@@ -3,24 +3,30 @@ hljs.initHighlightingOnLoad();
 $(document).ready(function(){
 
     var total_posts = 0;
+    var control_num = 0;
 
     $("h2,h3,h4,h5,h6").each(function(i,item){
        var tag = $(item).get(0).localName;
 
-       $(".date").each(function(i_a, item_a){
-         var tag_a = $(item_a).get(0).localName;
-         total_posts++;
-       });
+       if(control_num == 0){
+         $(".date").each(function(i_a, item_a){
+          total_posts++;
+         });
+       }
 
        $(item).attr("id","dir"+i);
        if($(item).attr("id") != "dir0" && $(item).text() != "목차 ")
           $("#category").append('<a style="color:black;" class="new'+tag+'" href="#dir'+i+'">'+$(this).text()+'</a></br>');
+
        $(".newh2").css("margin-left",25);
        $(".newh3").css("margin-left",45);
        $(".newh4").css("margin-left",65);
        $(".newh5").css("margin-left",85);
        $(".newh6").css("margin-left",105);
-       $("#category_count").html("총").append(i).append("개의 Categories, ").append(total_posts/7).append("개의 Posts");
+       $("#category_count").html("총").append(i).append("개의 Categories, ").append(total_posts).append("개의 Posts");
+
+       control_num++;
+
     });
 
     $("#category").hide();
